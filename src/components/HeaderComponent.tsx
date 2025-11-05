6085ab487049188e6c066989e242807c8cb2ce24
chickenchi <herosofthestom12@gmail.com>
2025-11-05 15:07:55 +0900

    Delete :: 필요 없는 컴포넌트 및 링크 제거

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -63,7 +63,6 @@ const HeaderComponent = () => {
         <LinkText href="/practice">연습</LinkText>
         <LinkText href="/check_mission">미션 확인</LinkText>
         <LinkText href="/pattern">공격 패턴</LinkText>
-        <LinkText href="/word_piece">글자 조각</LinkText>
       </Nav>
     </Header>
   );
